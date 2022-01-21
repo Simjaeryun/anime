bfbc3d3418c69454261b7ee698c0484751aa63d3
Simjaeryun <simjaeding@gmail.com>
2022-01-21 16:46:17 +0900

    %로 변환

--- js/anime.js
+++ js/anime.js
@@ -1,69 +1,54 @@
-
-
 const btn = document.querySelector('button');
 const box = document.querySelector('#box');
 
 btn.addEventListener('click', e => {
     anime(box, {
-        prop: "margin-top",
-        value: 300,
-
-        callback: () => {
-            anime(box, {
-                prop: "margin-left",
-                value: 1000,
-
-            })
-        }
+        prop: 'margin-left',
+        value: '10%',
+        duration: 2000
     });
 })
 
 
 function anime(selector, option) {
-    startTime = performance.now();
-    if (!option.duration) {
-        option.duration = 500;
+    const startTime = performance.now();
+    if (!option.duration) option.duration = 500;
+
+    let currentValue = parseInt(getComputedStyle(selector)[option.prop]);
+
+    //속성값이 문자열이면  %처리를 하기 위해 option.value값을 실수로 보정
+    let isString = typeof option.value;
+    if (isString === 'string') {
+        //px단위로 연산된 currentValue값을 다시 백분율로 변환처리
+        const winW = window.innerWidth;
+        currentValue = (currentValue / winW) * 100;
+        option.value = parseFloat(option.value);
     }
 
-    //현재 선택자의 걸려있는 속성값을 구해야함
-    const currentValue = parseInt(getComputedStyle(selector)[option.prop])
-    console.log(currentValue);
-    //현재값과 변경할값이 같으면 동작 중지
-    if (currentValue === option.value) {
-        return;
-    }
-    //현재값과 변경할값이 같지않으면 run함수 실행
+
+    if (currentValue === option.value) return;
     requestAnimationFrame(run);
 
     function run(time) {
         let timelast = time - startTime;
         let progress = timelast / option.duration;
 
-        if (progress < 0) {
-            progress = 0;
-        }
-
+        if (progress < 0) progress = 0;
         if (progress < 1) {
             requestAnimationFrame(run);
         } else {
-            if (option.callback) {
-                option.callback();
-            }
+            if (option.callback) option.callback();
         }
 
-        //현재 수치값에서 앞으로 변화가 되야하는 수치값만 progress연산처리 해줌
         const result = currentValue + ((option.value - currentValue) * progress);
-        selector.style[option.prop] = `${result}px`
+
+        //만약 isString값이 문자면 %적용
+        //그렇지 않으면 px적용
+        if (isString === 'string') {
+            selector.style[option.prop] = `${result}%`;
+        } else {
+            selector.style[option.prop] = `${result}px`;
+        }
+
     }
-
 }
-
-
-/*
-  requestAnimationFrame의 콜백함수 인수로 전달되는 값
-  - 해당 콜백함수가 반복 호출될때마다의 누적시간
-
-  진행률(백분율)
-  (현재진행수치 / 전체수치 ) * 100
-  */
-
