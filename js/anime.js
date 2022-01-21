02ba320ad608252493a68a9a3f566dd27f151826
Simjaeryun <simjaeding@gmail.com>
2022-01-21 15:56:44 +0900

    plus 함수 구현

--- js/anime.js
+++ js/anime.js
@@ -7,15 +7,38 @@ btn.addEventListener('click', e => {
     anime(box, {
         prop: "margin-top",
         value: 500,
-        duration: 1500
+
+        callback: () => {
+            anime(box, {
+                prop: "margin-left",
+                value: 500,
+
+            })
+        }
     });
 })
 
 
 function anime(selector, option) {
     startTime = performance.now();
-    requestAnimationFrame(move);
-    function move(time) {
+    if (!option.duration) {
+        option.duration = 500;
+    }
+
+    //현재 선택자의 걸려있는 속성값을 구해야함
+    const currentValue = parseInt(getComputedStyle(selector)[option.prop])
+    console.log(currentValue);
+    if (currentValue === option.value) {
+        return;
+    }
+    if (currentValue < option.value) {
+        requestAnimationFrame(plus);
+    }
+    if (currentValue > option.value) {
+        requestAnimationFrame(minus)
+    }
+
+    function plus(time) {
         let timelast = time - startTime;
         let progress = timelast / option.duration;
 
@@ -24,9 +47,36 @@ function anime(selector, option) {
         }
 
         if (progress < 1) {
-            requestAnimationFrame(move);
-            selector.style[option.prop] = `${option.value * progress}px`
+            requestAnimationFrame(plus);
+        } else {
+            if (option.callback) {
+                option.callback();
+            }
         }
+
+        //현재 수치값에서 앞으로 변화가 되야하는 수치값만 progress연산처리 해줌
+        const result = currentValue + ((option.value - currentValue) * progress);
+        selector.style[option.prop] = `${result}px`
+    }
+
+
+    function minus(time) {
+        let timelast = time - startTime;
+        let progress = timelast / option.duration;
+
+        if (progress < 0) {
+            progress = 0;
+        }
+
+        if (progress < 1) {
+            requestAnimationFrame(minus);
+        } else {
+            if (option.callback) {
+                option.callback();
+            }
+        }
+        const result =
+            selector.style[option.prop] = `${option.value * progress}px`
     }
 
 }
